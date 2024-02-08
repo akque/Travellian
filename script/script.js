cdbbccabed7c1f2e25de35fa04ae9e1be4240e59
akque <119449560+akque@users.noreply.github.com>
2024-02-08 16:18:31 +0300

    scripts

--- script/script.js
+++ script/script.js
@@ -22,3 +22,17 @@ function scrollSlide (next, gap, sliderContainer, nameCount) {
 
 sliderButtonRight.addEventListener("click", () => scrollSlide(true, 32, sliderContainer, 'destCount'))
 sliderButtonLeft.addEventListener("click", () => scrollSlide(false, 32, sliderContainer, 'destCount'))
+
+const form = document.querySelector(".main_form")
+const inputForm = form.querySelectorAll(".form_input")
+
+function formSubmit (event) {
+    event.preventDefault()
+    const obj = {}
+    for(let i = 0; i < inputForm.length; i++) {
+        obj[inputForm[i].name] = inputForm[i].value
+    }
+    console.log(obj)
+}
+
+form.addEventListener("submit", formSubmit)
